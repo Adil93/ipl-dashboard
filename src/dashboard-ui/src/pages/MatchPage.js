1d4a4c3fb1c82b428f9ae9bdcb969fb5d8f65be2
Adil93 <adil93.mec@gmail.com>
2021-07-03 18:54:09 +0530

    Calling match api from match page

--- src/dashboard-ui/src/pages/MatchPage.js
+++ src/dashboard-ui/src/pages/MatchPage.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import { MatchDetailCard } from 'components/MatchDetailCard';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const MatchPage = () => {
+
+    const emptyReponse = [
+        {
+            "id": 0,
+            "city": "",
+            "date": "",
+            "playerOfMatch": "",
+            "venue": "",
+            "team1": "",
+            "team2": "",
+            "tossWinner": "",
+            "tossDecision": "",
+            "winner": "",
+            "result": "",
+            "resultMargin": "",
+            "eliminator": null,
+            "umpire1": "",
+            "umpire2": ""
+        }];
+
+    const { name, year } = useParams();
+    const [matches, setMatches] = useState(emptyReponse);
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8085/team/${name}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
+        };
+
+        fetchMatches();
+    }, [name, year])
+
     return (
         <div className="MatchPage">
             <h1>Match Page</h1>
+            {
+                matches.map(match => <MatchDetailCard name={name} match={match} />)
+            }
         </div>
     );
 }
